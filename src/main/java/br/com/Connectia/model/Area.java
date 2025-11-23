bef39bd4abb603ee697f85e0e52b3140c90e1624
johnny-mathias <johnnydiasmathiasjunior@gmail.com>
2025-11-23 11:40:50 -0300

    Classe Area atualizada

--- src/main/java/br/com/Connectia/model/Area.java
+++ src/main/java/br/com/Connectia/model/Area.java
@@ -4,13 +4,17 @@ public class Area {
 
     private int idArea;
     private int idUsuario;
+    private String descricao;
+    private String tipoFormacao;
 
     public Area() {
     }
 
-    public Area (int idArea, int idUsuario) {
+    public Area (int idArea, int idUsuario, String descricao, String tipoFormacao) {
         this.idArea = idArea;
         this.idUsuario = idUsuario;
+        this.descricao = descricao;
+        this.tipoFormacao = tipoFormacao;
     }
 
     public int getIdArea() {
@@ -27,6 +31,22 @@ public class Area {
 
     public void setIdUsuario(int idUsuario) {
         this.idUsuario = idUsuario;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipoFormacao() {
+        return tipoFormacao;
+    }
+
+    public void setTipoFormacao(String tipoFormacao) {
+        this.tipoFormacao = tipoFormacao;
     }
 }
 
