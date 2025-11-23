652fd77e5a2b2abe42230b7731481c98d3bb496b
luisa-ganasevici <lganasevici@gmail.com>
2025-11-23 18:00:34 -0300

    possivel soluçaão render

--- src/main/java/br/com/Connectia/resource/CursoResource.java
+++ src/main/java/br/com/Connectia/resource/CursoResource.java
@@ -8,7 +8,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/")
+@Path("/cursos")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class CursoResource {
